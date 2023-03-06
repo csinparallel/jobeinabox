4537998097608d08d70a04d4320c6f9b3ae337f2
Dick Brown <rab@stolaf.edu>
2023-03-06 13:42:40 -0500

    pdc_task.php

---
+++ pdc_task.php
@@ -0,0 +1,54 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/* ==============================================================
+ *
+ * PDC
+ *
+ * ==============================================================
+ *
+ * @copyright  2014 Richard Lobb, University of Canterbury
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * modifications by rab@stolaf.edu March 2023
+ */
+
+require_once('application/libraries/LanguageTask.php');
+
+class PDC_Task extends Task {
+
+    public function __construct($filename, $input, $params) {
+        parent::__construct($filename, $input, $params);
+        $this->default_params['compileargs'] = array(
+            '-Wall',
+            '-Werror');
+    }
+
+    public static function getVersionCommand() {
+        return array('gcc --version', '/gcc \(.*\) ([0-9.]*)/');
+    }
+
+    public function compile() {
+        $src = basename($this->sourceFileName);
+        $this->executableFileName = $execFileName = "$src.exe";
+        $compileargs = $this->getParam('compileargs');
+        $linkargs = $this->getParam('linkargs');
+        $cmd = "g++ " . implode(' ', $compileargs) . " -o $execFileName $src " . implode(' ', $linkargs);
+        list($output, $this->cmpinfo) = $this->run_in_sandbox($cmd);
+    }
+
+
+    // A default name for C++ programs
+    public function defaultFileName($sourcecode) {
+        return 'prog.cpp';
+    }
+
+
+    // The executable is the output from the compilation
+    public function getExecutablePath() {
+        return "./" . $this->executableFileName;
+    }
+
+
+    public function getTargetFile() {
+        return '';
+    }
+};
